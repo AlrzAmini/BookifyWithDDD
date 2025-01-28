8317f390050e8cc6b4bed860a1c5ca588fdb38dc
mranotmillion@gmail.com <mranotmillion@gmail.com>
2025-01-29 02:31:38 +0330

    Modify: Entity now has collection of its domain events

--- Bookify.Domain/Abstractions/Entity.cs
+++ Bookify.Domain/Abstractions/Entity.cs
@@ -8,8 +8,27 @@ namespace Bookify.Domain.Abstractions
 {
     public abstract class Entity(Guid id, DateTime createdAt)
     {
+        private readonly List<IDomainEvent> _domainEvents = [];
+
+
         public Guid Id { get; init; } = id;
 
         public DateTime CreatedAt { get; init; } = createdAt;
+
+
+        public IReadOnlyCollection<IDomainEvent> GetDomainEvents()
+        {
+            return _domainEvents;
+        }
+
+        protected void RaiseDomainEvents(IDomainEvent domainEvent)
+        {
+            _domainEvents.Add(domainEvent);
+        }
+
+        public void ClearDomainEvents()
+        {
+            _domainEvents.Clear();
+        }
     }
 }
