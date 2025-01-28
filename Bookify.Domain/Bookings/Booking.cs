fe4ba56c1418c58a22db46bd25dfb8fa8681d932
mranotmillion@gmail.com <mranotmillion@gmail.com>
2025-01-29 02:33:52 +0330

    Add: Booking entity and its value-objects
    -- it does not completed yet

---
+++ Bookify.Domain/Bookings/Booking.cs
@@ -0,0 +1,93 @@
+﻿using Bookify.Domain.Abstractions;
+using Bookify.Domain.Apartments;
+using Bookify.Domain.Bookings.Events;
+
+namespace Bookify.Domain.Bookings;
+
+public sealed class Booking : Entity
+{
+    private Booking(
+        Guid id,
+        DateTime createdAt,
+        Guid apartmentId,
+        Guid userId,
+        DateRange dateRange,
+        Money priceForPeriod,
+        Money amenitiesUpCharge,
+        BookingStatus bookingStatus) : base(id, createdAt)
+    {
+        ApartmentId = apartmentId;
+        UserId = userId;
+        DateRange = dateRange;
+        PriceForPeriod = priceForPeriod;
+        AmenitiesUpCharge = amenitiesUpCharge;
+        Status = bookingStatus;
+    }
+
+    public Guid ApartmentId { get; private set; }
+
+    public Guid UserId { get; private set; }
+
+    public DateRange DateRange { get; private set; }
+
+    public Money PriceForPeriod { get; private set; }
+
+    public Money AmenitiesUpCharge { get; private set; }
+
+    public BookingStatus Status { get; private set; }
+
+    public static Booking Create(
+        Guid apartmentId,
+        Guid userId,
+        DateRange dateRange,
+        Money priceForPeriod,
+        Money amenitiesUpCharge)
+    {
+        var booking = new Booking(
+            Guid.CreateVersion7(),
+            DateTime.Now,
+            apartmentId,
+            userId,
+            dateRange,
+            priceForPeriod,
+            amenitiesUpCharge,
+            BookingStatus.Pending); // Assuming default status is Pending
+
+        booking.RaiseDomainEvents(new BookingCreatedDomainEvent(booking.Id));
+
+        return booking;
+    }
+
+    //public void ConfirmBooking()
+    //{
+    //    if (Status != BookingStatus.Pending)
+    //    {
+    //        throw new InvalidOperationException("Only pending bookings can be confirmed.");
+    //    }
+
+    //    Status = BookingStatus.Confirmed;
+    //    RaiseDomainEvents(new BookingConfirmedDomainEvent(Id));
+    //}
+
+    //public void CancelBooking()
+    //{
+    //    if (Status == BookingStatus.Canceled)
+    //    {
+    //        throw new InvalidOperationException("The booking is already canceled.");
+    //    }
+
+    //    Status = BookingStatus.Canceled;
+    //    RaiseDomainEvents(new BookingCanceledDomainEvent(Id));
+    //}
+
+    //public void UpdateAmenitiesUpCharge(Money newUpCharge)
+    //{
+    //    if (newUpCharge == null || newUpCharge.Amount < 0)
+    //    {
+    //        throw new ArgumentException("Amenities upcharge must be a valid non-negative value.");
+    //    }
+
+    //    AmenitiesUpCharge = newUpCharge;
+    //    RaiseDomainEvents(new BookingAmenitiesUpdatedDomainEvent(Id, newUpCharge));
+    //}
+}
