27f521ddb01881244a39ee0d724f448e88c15fe5
mranotmillion@gmail.com <mranotmillion@gmail.com>
2025-01-29 02:32:15 +0330

    Add: Repositories and UnitOfWork

---
+++ Bookify.Domain/Apartments/IApartmentRepository.cs
@@ -0,0 +1,10 @@
+﻿using Bookify.Domain.Users;
+
+namespace Bookify.Domain.Apartments;
+
+public interface IApartmentRepository
+{
+    Task<Apartment?> GetById(Guid id, CancellationToken cancellationToken = default);
+
+    Task<Guid> Add(Apartment apartment, CancellationToken cancellationToken = default);
+}
---
+++ Bookify.Domain/Users/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using Bookify.Domain.Apartments;
+
+namespace Bookify.Domain.Users;
+
+public interface IUserRepository
+{
+    Task<User?> GetById(Guid id, CancellationToken cancellationToken = default);
+
+    Task<Guid> Add(User user, CancellationToken cancellationToken = default);
+}
