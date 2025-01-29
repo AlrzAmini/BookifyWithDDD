d675d281e5d15b61ef1804010f419e2c492078fb
mranotmillion@gmail.com <mranotmillion@gmail.com>
2025-01-29 20:28:29 +0330

    Add: required services from application project

--- Bookify.Application/DependencyInjection.cs
+++ Bookify.Application/DependencyInjection.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Bookify.Domain.Bookings;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Bookify.Application
@@ -11,6 +12,13 @@ namespace Bookify.Application
     {
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
+            services.AddMediatR(configuration =>
+            {
+                configuration.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
+            });
+
+            services.AddTransient<PricingService>();
+
             return services;
         }
     }
