18590b328ea2a56f34537a4213f0243bd1b3262d
mranotmillion@gmail.com <mranotmillion@gmail.com>
2025-01-30 19:23:29 +0330

    Add: booking repository

---
+++ Bookify.Domain/Bookings/IBookingRepository.cs
@@ -0,0 +1,10 @@
+﻿using Bookify.Domain.Apartments;
+
+namespace Bookify.Domain.Bookings;
+
+public interface IBookingRepository
+{
+    Task<Guid> Add(Booking booking, CancellationToken cancellationToken);
+
+    Task<bool> IsOverlapping(Apartment apartment, DateRange dateRange, CancellationToken cancellationToken);
+}
