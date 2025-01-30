29f22298231c4e1e9f401fca7b12534314e7ecc4
mranotmillion@gmail.com <mranotmillion@gmail.com>
2025-01-30 20:26:25 +0330

    Add: get by id for booking repository

--- Bookify.Domain/Bookings/IBookingRepository.cs
+++ Bookify.Domain/Bookings/IBookingRepository.cs
@@ -4,6 +4,8 @@ namespace Bookify.Domain.Bookings;
 
 public interface IBookingRepository
 {
+    Task<Booking?> GetById(Guid id, CancellationToken cancellationToken);
+
     Task<Guid> Add(Booking booking, CancellationToken cancellationToken);
 
     Task<bool> IsOverlapping(Apartment apartment, DateRange dateRange, CancellationToken cancellationToken);
